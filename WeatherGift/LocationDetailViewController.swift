19916862d59b823217735d1f97576953b7ffc4f7
weaverth <weaverth@bc.edu>
2021-10-25 10:48:56 -0400

    6.16 Done!

--- WeatherGift/LocationDetailViewController.swift
+++ WeatherGift/LocationDetailViewController.swift
@@ -8,7 +8,6 @@
 import UIKit
 
 private let dateFormatter: DateFormatter = {
-    print("📆 I just created a date formatter!")
     let dateFormatter = DateFormatter()
     dateFormatter.dateFormat = "EEEE, MMM d"
     return dateFormatter
